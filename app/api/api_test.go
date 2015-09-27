b3d860f96469ce6a0f2fac2ff981ebba4fa1f7d9
Nate Woods <big.nate.w@gmail.com>
2015-09-26 22:14:13 -0600

    another test

--- app/api/api_test.go
+++ app/api/api_test.go
@@ -5,8 +5,19 @@ import (
   "testing"
 )
 
-func TestCountQuery(t *testing.T) {
+func TestCountQueryNegative(t *testing.T) {
   test_url, err := url.Parse("http://example.com/sub/path/file.txt?as=df&count=-1&apples=oranges")
+  if err != nil {
+    t.Error("Some error %v", err)
+  }
+  test_value := get_url_count(test_url)
+  if test_value != 2 {
+    t.Error("Incorrect result 2 !=", test_value)
+  }
+}
+
+func TestCountQueryPositive(t *testing.T) {
+  test_url, err := url.Parse("http://example.com/sub/path/file.txt?as=df&count=99&apples=oranges")
   if err != nil {
     t.Error("Some error %v", err)
   }
