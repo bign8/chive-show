b146e7f4b90e5b6a533c972dd73ec6d2f1133a4c
Nate Woods <big.nate.w@gmail.com>
2015-10-28 23:02:14 -0600

    Attempting to give good name to deploys (round 1)

--- script/ci/after_success.sh
+++ script/ci/after_success.sh
@@ -8,8 +8,10 @@ codecov
 # Deploy to appengine
 export GAE_DIR=../go_appengine
 export APP_DIR=.
-echo "PR# $TRAVIS_PULL_REQUEST"
-# TODO: use latest commit in part of deploy-id
-python $GAE_DIR/appcfg.py --oauth2_refresh_token=$GAE_OAUTH_REFRESH_TOKEN update $APP_DIR
+
+# TODO: make this version number suck less (only deploy on PR builds, etc...)
+export APP_VERSION="$TRAVIS_BRANCH-$TRAVIS_PULL_REQUEST"
+
+python $GAE_DIR/appcfg.py --oauth2_refresh_token=$GAE_OAUTH_REFRESH_TOKEN update $APP_DIR -V $APP_VERSION
 
 echo "SUCCESS!!!!"
