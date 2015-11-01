e70b32956ac68088b67796363f70ec13943fabdb
Nate Woods <big.nate.w@gmail.com>
2015-11-01 14:32:07 -0700

    Macking app work with new version of appengine builder

---
+++ yaml/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bign8/chive-show/app/api"
+	"github.com/bign8/chive-show/app/cron"
+)
+
+func init() {
+	http.HandleFunc("/", http.NotFound) // Default Handler
+
+	// Setup Other routes routes
+	api.Init()
+	cron.Init()
+}
