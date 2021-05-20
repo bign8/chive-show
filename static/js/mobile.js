ff2445e5eb3de2bfe17b107879de5ab2666a1933
Nate Woods <big.nate.w@gmail.com>
2021-05-20 12:58:40 +0000

    moar mobile

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -1,15 +1,33 @@
 let next_link = '/api/v1/post/random?count=10'
 let scroller = document.querySelector('.scroller')
 
+function create_tag(tag) {
+    // TODO: build user list of colored categories (and defaults)
+    // TODO: create category equivalency map (ie: DAR = Daily Afternoon ...)
+    const colors = ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'dark']
+    let color = colors[Math.floor(Math.random() * colors.length)]
+
+    let badge = document.createElement('span')
+    badge.innerText = tag
+    badge.classList.add('badge')
+    badge.classList.add('bg-' + color)
+    return badge
+}
+
 // convert json data into html data
 function create_post(post) {
     let div = document.createElement('div')
     div.classList.add('post')
 
+    let banner = document.createElement('div')
+    div.append(banner)
+
     let title = document.createElement('h5')
     title.innerText = post.title
     title.title = post.title
-    div.append(title)
+    banner.append(title)
+
+    for (let tag of post.tags) banner.append(create_tag(tag))
 
     let pre = document.createElement('pre')
     pre.innerText = JSON.stringify(post, null, 2)
@@ -26,7 +44,7 @@ function pump() {
     }).then(posts => {
         for (let post of posts) scroller.append(create_post(post))
         observer.observe(scroller.lastChild.previousSibling)
-    });
+    })
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
